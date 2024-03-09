1dea26a6b880bba912500c7c18fb5b8eb0731fbd
therealPatrick <malamashangala13@gmail.com>
2024-03-09 12:39:25 +0200

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+import { Application } from "@hotwired/stimulus"
+import Dropdown from 'stimulus-dropdown'
+
+
+const application = Application.start()
+application.register('dropdown', Dropdown)
