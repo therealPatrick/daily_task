2be025ff228897b34f10b741b4845b7bb5c16649
therealPatrick <malamashangala13@gmail.com>
2024-03-10 13:37:43 +0200

    Devise sign up and error helper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-
 import { Application } from "@hotwired/stimulus"
 import Dropdown from 'stimulus-dropdown'
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
 # Pin npm packages by running ./bin/importmap
-
+# pin_all_from "app/javascript/lib", under: "lib"
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-dropdown" # @2.1.0
 pin "hotkeys-js" # @3.13.7
 pin "stimulus-use" # @0.51.3
+# import "lib/flash"
