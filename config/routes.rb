942d941ca66fe56d65be8b9bb1b9209beb941d23
therealPatrick <malamashangala13@gmail.com>
2024-03-29 13:42:25 +0200

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/new'
+  get 'user/create'
+  get 'user/edit'
+  get 'user/update'
   devise_for :users,controllers: {registrations: "registrations"}
   resource :accounts
 
