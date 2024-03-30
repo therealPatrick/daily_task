b1165521aa4eeb291d3ff42d43b13767642553d1
therealPatrick <malamashangala13@gmail.com>
2024-03-30 15:41:58 +0200

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
+# Rails.application.routes.draw do
+#   devise_for :users, controllers: { registrations: "registrations" }
+#   resource :accounts
+
+#   ### Changes Start Here ###
+#   get 'user/me', to: 'users#me', as: 'my_settings'
+#   patch 'user/update_me', to: 'users#update_me', as: 'update_my_settings'
+#   get 'user/password', to: 'users#password', as: 'my_password'
+#   patch 'user/update_password', to: 'users#update_password', as: 'my_update_password'
+#   scope 'account', as: 'account' do
+#     resources :users
+#   end
+#   ### Changes End Here ###
+
+#   get 'activity/mine'
+#   get 'activity/feed'
+
+#   root to: 'activity#mine'
+# end
+
+
+
+
 Rails.application.routes.draw do
   get 'user/index'
   get 'user/show'
@@ -7,6 +30,15 @@ Rails.application.routes.draw do
   get 'user/update'
   devise_for :users,controllers: {registrations: "registrations"}
   resource :accounts
+
+  get 'user/me', to: 'users#me', as: 'my_settings'
+  patch 'user/update_me', to: 'users#update_me', as: 'upload_my_settings'
+  get 'user/password', to: 'users#password', as: 'my_password'
+  patch 'user/update_password', to: 'users#update_password', as: 'my_update_password'
+
+  scope 'account', as: 'account' do
+    resources :users
+  end
 
   get 'activity/mine'
   get 'activity/feed'
