a6b7a7e89c2cdee9c25c4c74c8be20db757500ad
therealPatrick <malamashangala13@gmail.com>
2024-03-30 14:08:13 +0200

    bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
 	before_action :authenticate_user!
 	layout :layout_by_resource
+
+	# helper method
+	helper_method :current_account
+
+	def current_account
+		@current_account ||= current_user.account
+		@current_account
+	end
 
 	protected
 	def layout_by_resource
